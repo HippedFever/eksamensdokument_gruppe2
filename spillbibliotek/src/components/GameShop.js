89d1db3d7ee8783f1f8a7f53435ed11313beefbd
rochlenge <77332328+rochlenge@users.noreply.github.com>
2023-04-29 17:46:37 +0200

    Fikset routing

---
+++ spillbibliotek/src/components/GameShop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GameShop() {
+  return (
+    <div>
+      <h1>GameShop</h1>
+      <p>Content for GameShop component</p>
+    </div>
+  );
+}
+
+export default GameShop;
---
+++ spillbibliotek/src/components/MyFavourites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyFavourites() {
+  return (
+    <div>
+      <h1>My Favourites</h1>
+      <p>Content for My Favourites component</p>
+    </div>
+  );
+}
+
+export default MyFavourites;
