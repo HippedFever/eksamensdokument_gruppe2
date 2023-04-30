f788a63391c21231f8aedb902dfb1da891317d33
rochlenge <77332328+rochlenge@users.noreply.github.com>
2023-04-30 20:05:32 +0200

    lagt inn pages, css og endre gamecards

--- spillbibliotek/src/components/Navbar.js
+++ spillbibliotek/src/components/Navbar.js
@@ -1,23 +1,24 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav>
-            <p>Vennligst velg en av følgende:</p>
-            <ul>
-                <li>
-                <Link to="/gameshop">Visit Shop</Link>
-                </li>
-                <li>
-                <Link to="/mygames">Go to library</Link>
-                </li>
-                <li>
-                <Link to="/myfavourites">Go to favorites</Link>
-                </li>
-            </ul>
-        </nav>
-    );
-  }
-  
-  
-  export default Navbar;
+  return (
+    <nav>
+      <Link to="/">
+        <h1 className="header-title">GameShop</h1>
+      </Link>
+      <ul>
+        <li>
+          <Link to="/gameshop">Visit Shop</Link>
+        </li>
+        <li>
+          <Link to="/mygames">Go to library</Link>
+        </li>
+        <li>
+          <Link to="/myfavourites">Go to favorites</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
