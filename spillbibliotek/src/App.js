65925dbeedf5d44c323b96b1bc931a1d369e14e6
rochlenge <77332328+rochlenge@users.noreply.github.com>
2023-04-30 17:39:40 +0200

    Rotet med CSS

--- spillbibliotek/src/App.js
+++ spillbibliotek/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import GameShop from './components/GameShop';
 import MyGames from './components/MyGames';
 import MyFavourites from './components/MyFavourites';
-import './App.css';
+import './css/main.css'
 
 function App() {
   return (
