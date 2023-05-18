1dcf4a16f6e7c43dabd22396c2f16132e0b57793
rochlenge <77332328+rochlenge@users.noreply.github.com>
2023-05-18 06:33:10 +0200

    Endret CSS for GameCards

--- spillbibliotek/src/components/GameCard.js
+++ spillbibliotek/src/components/GameCard.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 function GameCard({ title, description, hoverText, buttonText, backgroundImage }) {
+
+  const handleButtonClick = () => {
+    // Add your logic here for handling the button click
+    console.log("Button clicked");
+  };
+
   return (
     <article className="card">
       <div className="image-container">
-      <div className="pic" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+        <div className="pic" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
       </div>
       <div className="content">
         <h2>{title}</h2>
         <p>{description}</p>
         <div className="hover-text">{hoverText}</div>
-        <button>{buttonText}</button>
+        <button onClick={handleButtonClick}>{buttonText}</button>
       </div>
     </article>
   );
