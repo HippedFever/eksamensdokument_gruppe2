50e69c2799e40628027811b89dfcf388c238a536
HippedFever <Matsc99@gmail.com>
2023-05-18 23:59:21 +0200

    Første versjon av library

--- spillbibliotek/src/components/GameCard.js
+++ spillbibliotek/src/components/GameCard.js
@@ -2,26 +2,27 @@ import React from "react";
 import BuyButton from "./BuyButton";
 import Heart from "./Heart";
 
-function GameCard({ title, description, hoverText, backgroundImage, onButtonClick }) {
+
+function GameCard({ title, description, hoverText, backgroundImage, gameId, onBuyButtonClick }) {
   const handleHeartClick = (event) => {
     event.stopPropagation();
   };
 
-  const handleButtonClick = () => {
-    if (typeof onButtonClick === "function") {
-      onButtonClick();
+  const handleBuyButtonClick = () => {
+    if (typeof onBuyButtonClick === "function") {
+      onBuyButtonClick(gameId);
     }
   };
 
   return (
-    <article className="card" onClick={handleButtonClick}>
+    <article className="card">
       <Heart onClick={handleHeartClick} />
-        <div className="pic" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+      <div className="pic" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
       <div className="content">
         <h2>{title}</h2>
         <p>{description}</p>
         <div className="hover-text">{hoverText}</div>
-        <BuyButton buttonText="BUY" onButtonClick={handleButtonClick} />
+        <BuyButton buttonText="BUY" onButtonClick={handleBuyButtonClick} />
       </div>
     </article>
   );
