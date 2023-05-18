9d5e2b44cf37285c31ecd165ce9b8948ce85ca00
HippedFever <Matsc99@gmail.com>
2023-05-18 07:28:34 +0200

    Fikset spillbutikk på forside

--- spillbibliotek/src/components/GameShopFront.js
+++ spillbibliotek/src/components/GameShopFront.js
@@ -9,7 +9,7 @@ function GameStore() {
   useEffect(() => {
     async function fetchGames() {
       try {
-        const url = `https://api.rawg.io/api/games?key=27e0f70c258642ebab90d7b2680c5c4b&dates=2023-01-01,2023-12-31&metacritic=70,100&ordering=-metacritic&page_size=3`;
+        const url = `https://api.rawg.io/api/games?key=27e0f70c258642ebab90d7b2680c5c4b&dates=2023-01-01,2023-12-31&metacritic=70,100&ordering=-metacritic&page_size=5`;
         const response = await fetch(url);
         const data = await response.json();
         setGames(data.results);
