99079034b430d9ca04450d8050b58b876c73c0d1
rochlenge <77332328+rochlenge@users.noreply.github.com>
2023-05-19 04:39:07 +0200

    endret css

--- spillbibliotek/src/components/GameCard.js
+++ spillbibliotek/src/components/GameCard.js
@@ -21,12 +21,11 @@ function GameCard({ title, backgroundImage, gameId, onBuyButtonClick, onHeartCli
       <article className="card" >
         <Heart gameId={gameId} onClick={handleButtonClick} />
         <div className="pic" style={{ backgroundImage: `url(${backgroundImage})` }}>
-          
           <Link to={`/gameshop/games/${gameId}`} className="game-card-link">
           <button className="readButton">Read more!</button>
           </Link>
         </div>
-        <div className="content">
+        <div>
           <h2>{title}</h2>
           <BuyButton buttonText="BUY" onButtonClick={handleButtonClick} />
         </div>
